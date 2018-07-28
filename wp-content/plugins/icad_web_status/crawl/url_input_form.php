10a669eb1471baeb39b36b360a44246f52229b75
rshepard2278 <rshepard@student.ncmich.edu>
2018-07-28 08:28:53 -0400

    Complete yet simple functioning plugin. Just re-zip the icad_web_status folder and re-install elsewhere. Also removed unneccessary theme and pluin files.

--- wp-content/plugins/icad_web_status/crawl/url_input_form.php
+++ wp-content/plugins/icad_web_status/crawl/url_input_form.php
@@ -1,4 +1,4 @@
-		<form action="http://testing.local/crawl-test/" method="POST">
+		<form action='' method="POST">
 			URL : <input name="url" size="35" placeholder="Don't forget to add http:// or https"/>
 			<input type="submit" name="submit" value="Start Crawling"/>
 		</form>
--- wp-content/plugins/icad_web_status/crawl_html.php
+++ wp-content/plugins/icad_web_status/crawl_html.php
@@ -1,12 +1,13 @@
 <?php
-	include("crawl/url_input_form.php");
-	include("crawl/initial_crawl_page.php");
-	include("crawl/spider.class.php");
+	$root =  plugin_dir_path( __FILE__ );
+	include($root . "crawl/url_input_form.php");
+	include($root . "crawl/initial_crawl_page.php");
+	include($root . "crawl/spider.class.php");
 
 	$crawled_urls = array();
 	$found_urls = array();
 
-	include("crawl/crawl_functions.php");
+	include($root . "crawl/crawl_functions.php");
 
 	if(isset($_POST['submit'])){
 		$url = $_POST['url'];
