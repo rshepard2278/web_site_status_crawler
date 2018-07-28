c971762114fcd84f8fce8ceff1fc997e96dc479b
rshepard2278 <rshepard@student.ncmich.edu>
2018-07-28 08:15:31 -0400

    Removed unneeded files

--- wp-content/themes/twentyseventeen/crawl/url_input_form.php
+++
@@ -1,4 +0,0 @@
-		<form action="http://testing.local/crawl-test/" method="POST">
-			URL : <input name="url" size="35" placeholder="Don't forget to add http:// or https"/>
-			<input type="submit" name="submit" value="Start Crawling"/>
-		</form>
